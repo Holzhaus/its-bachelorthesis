9f503ee971b7a7690f30cfe6e15e39425beb5069
jholthuis <jholthuis@c8f4bf14-e3e7-0310-915e-fa211f8721cd>
2017-03-21 10:31:19 +0000

    code/xjcc: Add basic argparse and plugin structure
    git-svn-id: https://hgisvn.nds.ruhr-uni-bochum.de/svn/nds/repos_diplom/Bachelorarbeiten/2017/Jan_Holthuis@13668 c8f4bf14-e3e7-0310-915e-fa211f8721cd

--- code/xjcc/xjcc/main.py
+++ code/xjcc/xjcc/main.py
@@ -1,5 +1,50 @@
 # -*- coding: utf-8 -*-
+import argparse
+import logging
+from . import cli
+
 
 def main(args=None):
-    # TODO: Implement this
-    raise NotImplementedError
+    parser = argparse.ArgumentParser()
+
+    loglevel = parser.add_mutually_exclusive_group()
+    loglevel.add_argument(
+        '-v', '--verbose',
+        dest='loglevel',
+        action='store_const',
+        const=logging.INFO,
+        default=logging.WARNING,
+        help='Be verbose',
+    )
+    loglevel.add_argument(
+        '-vv', '--debug',
+        dest='loglevel',
+        action='store_const',
+        const=logging.DEBUG,
+        help='Even show debug messages'
+    )
+
+    loglevel.add_argument(
+        '-q', '--quiet',
+        dest='loglevel',
+        action='store_const',
+        const=100,
+        help='Be quiet',
+    )
+
+    subparsers = parser.add_subparsers(
+        dest='command',
+        )
+    subparsers.required = True
+
+    # list-converters
+    parser_list = subparsers.add_parser(
+        'list-converters',
+        help='list database contents'
+    )
+    parser_list.set_defaults(func=cli.list_converters)
+
+    # Parse arguments and execute code
+    p_args = parser.parse_args(args)
+    logging.basicConfig(level=p_args.loglevel)
+    p_args.func(p_args)
