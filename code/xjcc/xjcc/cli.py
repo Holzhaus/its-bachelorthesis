0aef3f72501ff05736cdc296d4509a129b90408b
jholthuis <jholthuis@c8f4bf14-e3e7-0310-915e-fa211f8721cd>
2017-05-29 11:00:56 +0000

    code/xjcc: Fix testresults sort key function
    git-svn-id: https://hgisvn.nds.ruhr-uni-bochum.de/svn/nds/repos_diplom/Bachelorarbeiten/2017/Jan_Holthuis@13892 c8f4bf14-e3e7-0310-915e-fa211f8721cd

--- code/xjcc/xjcc/cli.py
+++ code/xjcc/xjcc/cli.py
@@ -34,7 +34,7 @@ def get_testcases(name=None, category=None):
 
 
 def sort_testresults(row):
-    return '-'.join([row['converter'], row['testcase']])
+    return '-'.join([row['Converter'], row['Testcase']])
 
 
 def list_converters(args):
