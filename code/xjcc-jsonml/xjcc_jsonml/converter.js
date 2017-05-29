d41e170d9af094fff012d6742147ecb1595a5e06
jholthuis <jholthuis@c8f4bf14-e3e7-0310-915e-fa211f8721cd>
2017-05-29 11:01:08 +0000

    code/xjcc-jsonml: Use node-xjcc in xjcc-jsonml
    git-svn-id: https://hgisvn.nds.ruhr-uni-bochum.de/svn/nds/repos_diplom/Bachelorarbeiten/2017/Jan_Holthuis@13906 c8f4bf14-e3e7-0310-915e-fa211f8721cd

--- code/xjcc-jsonml/xjcc_jsonml/converter.js
+++ code/xjcc-jsonml/xjcc_jsonml/converter.js
@@ -3,6 +3,7 @@ const fs = require("fs")
 const path = require("path")
 const vm = require("vm")
 const xmldom = require("xmldom")
+const xjcc = require("xjcc");
 
 /* Create fake browser context by using methods from xmldom module */
 const ctx = {
@@ -19,16 +20,10 @@ let filename = require.resolve("jsonml-tools/jsonml-xml.js")
 let code = fs.readFileSync(filename, "utf-8");
 vm.runInNewContext(code, ctx);
 
-let to_xml = process.argv.includes("-d") || process.argv.includes("--decode");
-
-process.stdin.setEncoding("utf8");
-process.stdin.on("readable", () => {
-    let chunk = process.stdin.read();
-    if (chunk !== null) {
-        if (to_xml) {
-            process.stdout.write(ctx.JsonML.toXMLText(JSON.parse(chunk)));
-        } else {
-            process.stdout.write(JSON.stringify(ctx.JsonML.fromXMLText(chunk)));
-        }
+xjcc.process_input(function(data, encoding) {
+    if (xjcc.to_xml()) {
+        return ctx.JsonML.toXMLText(JSON.parse(data));
+    } else {
+        return JSON.stringify(ctx.JsonML.fromXMLText(data));
     }
 });
