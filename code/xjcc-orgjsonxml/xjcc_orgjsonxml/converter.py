e0d5e340fc139175aaf5f68d896084ee0195391a
jholthuis <jholthuis@c8f4bf14-e3e7-0310-915e-fa211f8721cd>
2017-06-13 00:20:34 +0000

    code/xjcc-{jsonlib|orgjsonxml}: Work around Java Memory Allocation issue
    git-svn-id: https://hgisvn.nds.ruhr-uni-bochum.de/svn/nds/repos_diplom/Bachelorarbeiten/2017/Jan_Holthuis@14103 c8f4bf14-e3e7-0310-915e-fa211f8721cd

--- code/xjcc-orgjsonxml/xjcc_orgjsonxml/converter.py
+++ code/xjcc-orgjsonxml/xjcc_orgjsonxml/converter.py
@@ -9,14 +9,30 @@ class OrgJsonXMLPlugin(xjcc.plugins.ConverterPlugin):
     """
     JAR_PATH = 'orgjsonxml/target/orgjsonxml.jar'
 
+    # Looks like java hates being in a restricted environment. It will fail
+    # with this error message when vmem is restricted.
+    #     Error occurred during initialization of VM
+    #     Could not allocate metaspace: 1073741824 bytes
+    #
+    # See this bug report: http://bugs.java.com/view_bug.do?bug_id=8043516
+    # We need to set a bunch args to make it work, but hey, the JAVA devs
+    # closed it as WONTFIX. Whatever.
+
+    JAVA_ARGS = [
+        '-Xms50m',
+        '-Xmx50m',
+        '-XX:MaxHeapSize=50m',
+        '-XX:CompressedClassSpaceSize=10m',
+    ]
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.java_app = self.get_package_filename(self.JAR_PATH)
 
     def xml_to_json(self, xml_data):
-        cmd = ['java', '-jar', self.java_app]
+        cmd = ['java', *self.JAVA_ARGS, '-jar', self.java_app]
         return self.run_command(cmd, xml_data)
 
     def json_to_xml(self, json_data):
-        cmd = ['java', '-jar', self.java_app, '--decode']
+        cmd = ['java', *self.JAVA_ARGS, '-jar', self.java_app, '--decode']
         return self.run_command(cmd, json_data)
