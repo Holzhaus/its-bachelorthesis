8ea39d8ea68ad83ebea139cc49576e9fbdce7a1b
jholthuis <jholthuis@c8f4bf14-e3e7-0310-915e-fa211f8721cd>
2017-06-18 17:44:30 +0000

    code/xjcc: Improve backwards compatiblity of JS converters with old Node versions
    git-svn-id: https://hgisvn.nds.ruhr-uni-bochum.de/svn/nds/repos_diplom/Bachelorarbeiten/2017/Jan_Holthuis@14159 c8f4bf14-e3e7-0310-915e-fa211f8721cd

--- code/xjcc-x2js/xjcc_x2js/converter.js
+++ code/xjcc-x2js/xjcc_x2js/converter.js
@@ -1,8 +1,8 @@
-let X2JS = require('x2js')
-let xjcc = require('xjcc')
+const X2JS = require('x2js')
+const xjcc = require('xjcc')
 
 xjcc.process_input(function(data, encoding) {
-    let x2js = new X2JS({
+    var x2js = new X2JS({
         "stripWhitespaces": false,
         "skipEmptyTextNodesForObj": false
     })
